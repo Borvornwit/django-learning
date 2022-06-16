56274f2d891fc3c687b985d3dc46e3b9b71cdf90
Pete <petebest47@gmail.com>
2022-06-16 16:38:19 +0700

    count word

--- app/views.py
+++ app/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {"name": "Patrick", "age": 23, "nationality": 'British'}
+    context = {"name": "Patrick", "age": 23, "nationality": "British"}
     return render(request, "index.html", context=context)
+
+
+def counter(request):
+    words = request.GET["words"]
+    num_words = len(words.split())
+    return render(request, "counter.html", {"num_words": num_words})
